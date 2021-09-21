a283bd993902d970b44174929581f562af3d2dba
Tomás Schaefer Borges de Jesus <tomassbj@gmail.com>
2021-09-21 20:56:34 -0300

    Commit inicial

---
+++ Calculadora/src/Application.java
@@ -0,0 +1,11 @@
+public class Application
+{
+    public static void main(String[] args)
+    {
+        /*
+            Implementação de calculadora de conversor de bases numéricas, com detalhamento do código.
+         */
+
+
+    }
+}
