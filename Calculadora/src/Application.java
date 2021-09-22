d10422edcc0f2d95b91dbe1afb787fd418c8e3f5
Gabaunzito <gabrielpmachado.contato@gmail.com>
2021-09-22 01:12:45 -0300

    Adicionado conversor de Binario para Decimal.

--- Calculadora/src/Application.java
+++ Calculadora/src/Application.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Application
 {
     public static void main(String[] args)
     {
+        Scanner teclado = new Scanner(System.in);
+        BinarioParaDecimal binarioDecimal = new BinarioParaDecimal();
+
+        /* Gabriel Puziski machado
+        System.out.println("Digite um numero para converter de binario para decimal: ");
+        int binario = teclado.nextInt(); <------- Recebe o Binario
+        System.out.println("Valor em decimal: " + binarioDecimal.converterBinarioDecimal(binario)); < ----------Chama o metodo que converte o numero de Binario para decimal
+        */
+
+
         /*
             Implementação de calculadora de conversor de bases numéricas, com detalhamento do código.
          */
---
+++ Calculadora/src/BinarioParaDecimal.java
@@ -0,0 +1,16 @@
+public class BinarioParaDecimal {
+
+    //Gabriel Puziski Machado
+
+    public static int converterBinarioDecimal(int binario ){
+        int i = 1, decimal = 0, memoria = 0;
+        while(binario!=0){
+            memoria = binario % 10;
+            decimal = decimal +memoria*i;
+            i=i*2;
+            binario = binario/10;
+
+        }
+        return decimal;
+    }
+}
