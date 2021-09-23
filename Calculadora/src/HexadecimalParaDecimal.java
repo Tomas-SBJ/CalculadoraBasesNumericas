9cac82ccc3a858ff49e764fbe93fcb56fc2d3127
Anthony <60969070+anthonyzutter@users.noreply.github.com>
2021-09-23 13:44:42 -0300

    Envio de nova classe

---
+++ Calculadora/src/HexadecimalParaDecimal.java
@@ -0,0 +1,20 @@
+/**
+ * Classe que converte Hexadecimal Para Decimal.
+ *
+ * @author Anthony de Zutter
+ */
+public class HexadecimalParaDecimal {
+
+    public static long converter(String hex) {
+        hex = hex.toUpperCase();
+        String digitos = "0123456789ABCDEF";
+        long decimal = 0;
+        for (int i = 0; i < hex.length(); i++) {
+            char c = hex.charAt(i);
+            int d = digitos.indexOf(c);
+            decimal = 16 * decimal + d;
+        }
+
+        return decimal;
+    }
+}
