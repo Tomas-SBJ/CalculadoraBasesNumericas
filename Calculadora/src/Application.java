4ccb6204bcf939a7f836903b0016ae2335ad9f35
Reinaldo0 <ju.naldo@hotmail.com>
2021-10-18 20:33:36 -0300

    Atualização na classe Main.

--- Calculadora/src/Application.java
+++ Calculadora/src/Application.java
@@ -5,19 +5,43 @@ public class Application
     public static void main(String[] args)
     {
         Scanner teclado = new Scanner(System.in);
-        BinarioParaDecimal binarioDecimal = new BinarioParaDecimal();
+        int opcao;
 
-        /* Gabriel Puziski machado
-        System.out.println("Digite um numero para converter de binario para decimal: ");
-        int binario = teclado.nextInt(); <------- Recebe o Binario
-        System.out.println("Valor em decimal: " + binarioDecimal.converterBinarioDecimal(binario)); < ----------Chama o metodo que converte o numero de Binario para decimal
-        */
+        System.out.println("""
+               Escolha uma opção: 
+               1- Converter
+               2- Calcular
+               """);
+        opcao = teclado.nextInt();
 
+        switch (opcao)
+        {
+            case 1 ->
+                    {
+                        System.out.println("""
+               Escolha uma opção: 
+               1- Converter
+               2- Calcular
+               """);
+                        opcao = teclado.nextInt();
+                    }
+            case 2 ->
+                    {
 
-        /*
-            Implementação de calculadora de conversor de bases numéricas, com detalhamento do código.
-         */
+                    }
 
+        }
 
+        System.out.println("Digite o 1º Numero hexadecimal:");
+        String numero1 = teclado.nextLine();
+
+        System.out.println("Digite o 2º Numero hexadecimal:");
+        String numero2 = teclado.nextLine();
+
+        long hexadecimal1 = HexadecimalParaDecimal.converter(numero1);
+        long hexadecimal2 = HexadecimalParaDecimal.converter(numero2);
+
+        System.out.println(hexadecimal1);
+        System.out.println(hexadecimal2);
     }
 }
